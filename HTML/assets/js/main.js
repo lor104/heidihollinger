1f83b63ba8b5845e9270abd5bebe66a0ae67fdfc
lor104 <lauren.rosentzveig@gmail.com>
2018-03-13 16:20:46 -0400

    add testing containers to allow for js function fade in for image

---
+++ HTML/assets/js/main.js
@@ -0,0 +1,12 @@
+$( document ).ready(function() {
+
+  window.addEventListener("load", function() {
+    console.log("loaded")
+    var loader = document.getElementById("loader");
+
+
+    loader.style.opacity = 0;
+  })
+
+
+})
